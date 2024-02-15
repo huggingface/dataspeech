82c42a7de9dddc675fc65093261121056093e8fd
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-02-15 14:04:11 +0000

    change main logic to make it faster and lighter

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
     
-    parser.add_argument("dataset_name", type=str, help="Repo id or local path.")
+    parser.add_argument("dataset_name", type=str, help="Repo id.")
     parser.add_argument("--configuration", default=None, type=str, help="Dataset configuration to use.")
     parser.add_argument("--dump_folder_path", default=None, type=str, help="If specified, save the dasaset on disk.")
     parser.add_argument("--repo_id", default=None, type=str, help="If specified, push the model to the hub.")
@@ -20,6 +20,8 @@ if __name__ == "__main__":
     parser.add_argument("--text_column_name", default="text", type=str, help="Text column name.")
     parser.add_argument("--rename_column", action="store_true", help="If activated, rename audio and text column names to 'audio' and 'text'. Useful if you wan't to merge datasets afterwards.")
     parser.add_argument("--cpu_num_workers", default=1, type=int, help="Number of CPU workers for transformations that don't use GPUs or if no GPU are available.")
+    parser.add_argument("--cpu_writer_batch_size", default=1000, type=int, help="writer_batch_size for transformations that don't use GPUs. See: https://huggingface.co/docs/datasets/v2.17.0/en/package_reference/main_classes#datasets.Dataset.map.writer_batch_size")
+    parser.add_argument("--batch_size", default=16, type=int, help="Batch size when relevant") # TODO: better description
 
     args = parser.parse_args()
     
@@ -35,39 +37,45 @@ if __name__ == "__main__":
 
 
     logger.info("Compute speaking rate")
-    updated_dataset = dataset.map(
+    rate_dataset = dataset.map(
         rate_apply,
         with_rank=False,
         num_proc=args.cpu_num_workers,
+        writer_batch_size= args.cpu_writer_batch_size,
+        remove_columns=[audio_column_name]
     )
 
     logger.info("Compute snr and reverb")
-    updated_dataset = updated_dataset.map(
+    snr_dataset = dataset.map(
         snr_apply,
         batched=True,
-        batch_size=16,
+        batch_size=args.batch_size,
         with_rank=True if torch.cuda.device_count()>0 else False,
-        num_proc=torch.cuda.device_count() if torch.cuda.device_count()>0 else args.cpu_num_workers
+        num_proc=torch.cuda.device_count() if torch.cuda.device_count()>0 else args.cpu_num_workers,
+        remove_columns=[audio_column_name]
     )
     
     logger.info("Compute pitch")
-    updated_dataset = updated_dataset.map(
+    pitch_dataset = dataset.map(
         pitch_apply,
         batched=True,
-        batch_size=16,
+        batch_size=args.batch_size,
         with_rank=True if torch.cuda.device_count()>0 else False,
-        num_proc=torch.cuda.device_count() if torch.cuda.device_count()>0 else args.cpu_num_workers
+        num_proc=torch.cuda.device_count() if torch.cuda.device_count()>0 else args.cpu_num_workers,
+        remove_columns=[audio_column_name]
     )
-    
-    updated_dataset = updated_dataset.remove_columns(audio_column_name)
+        
+    for split in dataset.keys():
+        dataset[split] = pitch_dataset[split].add_column("snr", snr_dataset[split]["snr"]).add_column("c50", snr_dataset[split]["c50"])
+        dataset[split] = dataset[split].add_column("speaking_rate", rate_dataset[split]["speaking_rate"]).add_column("phonemes", rate_dataset[split]["phonemes"])
     
     if args.dump_folder_path:
         logger.info("Saving to disk...")
-        updated_dataset.save_to_disk(args.dump_folder_path)
+        dataset.save_to_disk(args.dump_folder_path)
     if args.repo_id:
         logger.info("Pushing to the hub...")
         if args.configuration:
-            updated_dataset.push_to_hub(args.repo_id, args.configuration)
+            dataset.push_to_hub(args.repo_id, args.configuration)
         else:
-            updated_dataset.push_to_hub(args.repo_id)
+            dataset.push_to_hub(args.repo_id)
     
