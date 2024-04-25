8ea19740c7a207e04f039d0be3d3a4dae1ed4c07
sanchit-gandhi <sanchit@huggingface.co>
2024-04-25 10:23:14 +0100

    actually use dummy

--- examples/prompt_creation/run_prompt_creation_dummy.sh
+++ examples/prompt_creation/run_prompt_creation_dummy.sh
@@ -4,9 +4,10 @@ python run_prompt_creation.py \
   --dataset_name "ylacombe/libritts_r_tags_and_text" \
   --dataset_config_name "clean" \
   --dataset_split_name "dev.clean" \
-  --model_name_or_path "mistralai/Mistral-7B-Instruct-v0.2" \
+  --model_name_or_path "hf-internal-testing/tiny-random-LlamaForCausalLM" \
   --per_device_eval_batch_size 2 \
   --attn_implementation "sdpa" \
+  --torch_compile \
+  --max_eval_samples 8 \
   --dataloader_num_workers 0 \
-  --output_dir "./" \
-  --load_in_4bit
+  --output_dir "./"
