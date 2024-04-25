c194d901cf4366ef35a97f696e7764002d4f460c
sanchit-gandhi <sanchit@huggingface.co>
2024-04-25 17:03:38 +0100

    add slurm scripts

--- examples/prompt_creation_llm_swarm/run_prompt_creation_dummy.sh
+++ examples/prompt_creation_llm_swarm/run_prompt_creation_dummy.sh
@@ -4,7 +4,7 @@ python run_prompt_creation_llm_swarm.py \
   --dataset_name "ylacombe/libritts_r_tags_and_text" \
   --dataset_config_name "clean" \
   --dataset_split_name "dev.clean" \
-  --model_name_or_path "meta-llama/Llama-2-13b-hf" \
+  --model_name_or_path "meta-llama/Llama-2-7b-hf" \
   --output_dir "./" \
-  --max_eval_samples 1000 \
+  --max_eval_samples 1999 \
   --debug_endpoint "http://26.0.173.246:59699"
