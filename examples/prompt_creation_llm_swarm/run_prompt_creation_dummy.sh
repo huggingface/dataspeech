a59961689806dbc38789add62799ca72fc080d43
sanchit-gandhi <sanchit@huggingface.co>
2024-04-25 16:43:41 +0100

    update debugging

--- examples/prompt_creation_llm_swarm/run_prompt_creation_dummy.sh
+++ examples/prompt_creation_llm_swarm/run_prompt_creation_dummy.sh
@@ -4,8 +4,7 @@ python run_prompt_creation_llm_swarm.py \
   --dataset_name "ylacombe/libritts_r_tags_and_text" \
   --dataset_config_name "clean" \
   --dataset_split_name "dev.clean" \
-  --model_name_or_path "mistralai/Mistral-7B-Instruct-v0.1" \
+  --model_name_or_path "meta-llama/Llama-2-13b-hf" \
   --output_dir "./" \
-  --max_eval_samples 4 \
-  --debug_endpoint "http://26.0.161.142:26460" \
-  --per_instance_max_parallel_requests 2
+  --max_eval_samples 1000 \
+  --debug_endpoint "http://26.0.173.246:59699"
