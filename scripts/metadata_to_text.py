1b88d8bbea429d99caf3744c6c7a2126cd2ff5f4
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-06-10 20:10:53 +0000

    rename v2 si_sdr bins

--- scripts/metadata_to_text.py
+++ scripts/metadata_to_text.py
@@ -12,7 +12,7 @@ SPEAKER_RATE_BINS = ["very slowly", "quite slowly", "slightly slowly", "moderate
 SNR_BINS = ["very noisy", "quite noisy", "slightly noisy", "moderate ambient sound", "slightly clear", "quite clear", "very clear"]
 REVERBERATION_BINS = ["very roomy sounding", "quite roomy sounding", "slightly roomy sounding", "moderate reverberation", "slightly confined sounding", "quite confined sounding", "very confined sounding"]
 UTTERANCE_LEVEL_STD = ["very monotone", "quite monotone", "slightly monotone", "moderate intonation", "slightly expressive", "quite expressive", "very expressive"]
-SI_SDR_BINS = ["very noisy", "noisy", "slightly noisy", "almost no noise", "clear", "very clear"]
+SI_SDR_BINS = ["extremely noisy", "very noisy", "noisy", "slightly noisy", "almost no noise", "very clear"]
 PESQ_BINS = ["very bad speech quality", "bad speech quality", "slightly bad speech quality", "moderate speech quality", "great speech quality", "wonderful speech quality"]
 
 # this one is supposed to be apply to speaker-level mean pitch, and relative to gender
